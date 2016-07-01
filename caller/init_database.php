d1c24747b4129d3abdd1990e3a68b9e55562d4b8
Ley21 <andreas.spuling@web.de>
2016-07-01 08:29:41 +0000

    fix

--- caller/init_database.php
+++ caller/init_database.php
@@ -1,6 +1,6 @@
 <?php
-    require_once "config.php";
-    require_once "helper.php";
+    require_once "../config.php";
+    require_once "../helper.php";
     
     // Create table for minions 
     $database->query("CREATE TABLE minions (
