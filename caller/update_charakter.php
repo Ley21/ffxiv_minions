3ded2c52b4be085591a778825a663da7fefc9e2a
Ley21 <andreas.spuling@web.de>
2016-07-07 13:10:01 +0000

    add name and server du update php

--- caller/update_charakter.php
+++ caller/update_charakter.php
@@ -6,6 +6,8 @@
     
     $all = $_GET["all"];
     $numbers = $_GET["numbers"];
+    $name = $_GET["name"];
+    $server = $_GET["server"];
     
     if($numbers == "true"){
         $players = $database->select("players",["id"],"WHERE DATE_SUB(CURDATE(),INTERVAL 14 DAY) >= last_update_date");
@@ -14,6 +16,9 @@
             
             
         }
+    }
+    else if(!empty($name)){
+        echo insert_update_charakter_by_name($name,$server);
     }
     else{
         $id = $_GET["id"];
