d2211d25529745370499b79f88adacb409ea5143
Ley21 <andreas.spuling@web.de>
2016-11-07 09:56:09 +0000

    missing file

--- handler/faq.php
+++ handler/faq.php
@@ -5,6 +5,7 @@
     $html .= "<h3><b>".get_language_text("question_1")."</b></h3><p>".get_language_text("answer_1")."</p></br>";
     $html .= "<h3><b>".get_language_text("question_2")."</b></h3><p>".get_language_text("answer_2")."</p></br>";
     $html .= "<h3><b>".get_language_text("question_3")."</b></h3><p>".get_language_text("answer_3")."</p></br>";
+    $html .= "<h3><b>".get_language_text("question_4")."</b></h3><p>".get_language_text("answer_4")."</p></br>";
     
     echo $html;
 ?>
