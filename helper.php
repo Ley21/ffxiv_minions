797ab973e27207f6d43b32572f32b33c1f55fe1b
Ley21 <andreas.spuling@web.de>
2017-05-10 07:38:47 +0000

    add new informations to json files and add to helper

--- helper.php
+++ helper.php
@@ -688,6 +688,8 @@
             description_fr TEXT NOT NULL,
             description_de TEXT NOT NULL,
             description_ja TEXT NOT NULL,
+            sellable tinyint(1),
+            emotes VARCHAR(100),
             PRIMARY KEY ( id )
             );");
         
@@ -705,6 +707,7 @@
             description_de TEXT NOT NULL,
             description_ja TEXT NOT NULL,
             can_fly tinyint(1),
+            sellable tinyint(1),
             PRIMARY KEY ( id )
             );");
         $database->query("CREATE TABLE players (
@@ -784,6 +787,10 @@
                 if($table == "mounts"){
                     $database->update($table,["can_fly" => $coll->can_fly],["id[=]"=>$coll->id]);
                 }
+                if($table == "minions"){
+                    $database->update($table,["emotes" => $coll->emotes],["id[=]"=>$coll->id]);
+                }
+                $database->update($table,["sellable" => $coll->sellable],["id[=]"=>$coll->id]);
                 /*if(empty($coll->methodes) 
                     || count($coll->methodes) != $database->count($method_table,["m_id[=]"=>$coll->id])){
                     $database->delete($method_table,["m_id[=]"=>$coll->id]);
@@ -819,8 +826,8 @@
                         "method_description_fr","method_description_de","method_description_ja"],["m_id[=]"=>$obj["id"]]);
                 $obj_methodes = $obj_methodes ? $obj_methodes : array(array("method"=>null,"available"=>0,"method_description_en"=>null,
                         "method_description_fr"=>null,"method_description_de"=>null,"method_description_ja"=>null));
-                $method = $table == "minions" ? array("id"=>$obj["id"],"name"=>$obj["name"],"methodes"=>$obj_methodes) 
-                            : array("id"=>$obj["id"],"name"=>$obj["name"],"can_fly" => $obj["can_fly"],"methodes"=>$obj_methodes) ;
+                $method = $table == "minions" ? array("id"=>$obj["id"],"name"=>$obj["name"],"sellable"=>$obj["sellable"],"emotes"=>$obj["emotes"],"methodes"=>$obj_methodes) 
+                            : array("id"=>$obj["id"],"name"=>$obj["name"],"sellable"=>$obj["sellable"],"can_fly" => $obj["can_fly"],"methodes"=>$obj_methodes) ;
                 $methodes[] = $method;
             }
             $json_informations = json_encode($methodes,JSON_PRETTY_PRINT);
@@ -886,6 +893,11 @@
         }
     
         return (substr($haystack, -$length) === $needle);
+    }
+    
+    function is_mobile(){
+    	$useragent=$_SERVER['HTTP_USER_AGENT'];
+    	return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4));
     }
 ?>
         
