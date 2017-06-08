44368ccffbaf3f58f7019ffb9ff47c23c626731b
Ley21 <andreas.spuling@web.de>
2017-06-08 17:04:57 +0000

    fix delete

--- handler/fc_ranking.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "../config.php";
-    require_once "../helper.php";
-    
-    $fc_id = $_GET['fc'];
-    echo create_ranking("",$fc_id);
-
-?>
--- python_scripts/update_chars.py
+++
@@ -1,8 +0,0 @@
-import requests
-baseurl = 'http://ffxivcollector.com/'
-while True:
-    page = requests.get(baseurl+'handler/charakter.php?update_mode=true&show=false').text
-    print(page)
-    if "Finish" in page:
-        break
-
