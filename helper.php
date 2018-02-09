bff41df8250e091fa33d06b8d56416fee3bec644
Ley21 <andreas.spuling@web.de>
2018-02-09 07:37:28 +0000

    fix search

--- helper.php
+++ helper.php
@@ -462,7 +462,7 @@
         $characters = $api->searchCharacter($name, $server);
         
         // loop through characters
-        foreach($characters['results'] as $char) {
+        foreach($characters->getresults() as $char) {
             $char = (Object)$char;
             if($char->getserver() == $server && 
                 $char->getname() == $name){
